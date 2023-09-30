4163dbb515aa7e3d1ae5b5d23eb78fba9df9a6a4
Erasmo <erasmolpa@gmail.com>
2023-09-30 11:14:39 +0100

    refactoring

--- provider.tf
+++ provider.tf
@@ -1,7 +1,14 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
 
 # Configure the GitHub Provider
 provider "github" {
-  version      = "~> 2.2"
   organization = "${var.github_organization}"
   token        = "${var.github_token}"
   # Export the GITHUB_TOKEN as an environment variable
--- var.tf
+++ var.tf
@@ -2,8 +2,10 @@
 variable "github_token" {
   type = string
   sensitive = true
+  default = "XXXX"
 }
 
 variable "github_organization" {
   type = string
+  default = "org"
 }
