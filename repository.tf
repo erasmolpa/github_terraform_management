c08ab42ad8f248e0aab70ef7f041d50948b82999
Erasmo <erasmolpa@gmail.com>
2023-10-06 18:15:02 +0100

    settings

--- repository.tf
+++ repository.tf
@@ -3,6 +3,15 @@ resource "github_repository" "github-management" {
   name               = "repo-created-with-terraform"
   description        = "Terraform based repository"
   visibility         = "internal"
+  security_and_analysis {
+    secret_scanning {
+      status = "enabled"
+    }
+    secret_scanning_push_protection {
+      status = "enabled"
+    }
+  }
+  archive_on_destroy = true
   has_issues         = true
   has_wiki           = false
   allow_merge_commit = false
@@ -11,5 +20,6 @@ resource "github_repository" "github-management" {
   auto_init          = false
   license_template   = "mit"
   gitignore_template = "VisualStudio"
+  is_template        = true
   topics             = ["config", "terraform"]
 }
